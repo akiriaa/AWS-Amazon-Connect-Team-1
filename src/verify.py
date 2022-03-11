460d6eef1a4957ff8cb26d3f6484e7b400a5b712
Akiria A <akiria@Akirias-MacBook-Air.local>
2022-03-11 16:22:20 -0600

    removed customer attributes

--- src/verify.py
+++ src/verify.py
@@ -5,8 +5,7 @@ CONNECT_SECRET = "4YBiUClYL5mMINxlH+K9pPPe7Itq+W4iO3kdjAgxhS0="
 payload = {
   'sub': '15fa5871-0412-4c4f-9d8f-ab63d671f2cf',
   'iat': datetime.datetime.utcnow(),
-  'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=500),
-  'attributes': {"name": "Jane", "memberID": "123456789", "email": "Jane@example.com", "isPremiumUser": "true", "age": "45"}
+  'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=500)
 }
 
 header = {
@@ -15,4 +14,3 @@ header = {
 }
 
 encoded_token = jwt.encode((payload), CONNECT_SECRET, algorithm="HS256", headers=header)
-});
